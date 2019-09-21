2db249d099183e5cc36e816870fe40f577e63e2c
乔阳 <sTone@jinxianshengdeMacBook-Pro.local>
2019-09-21 16:16:14 +0800

    docs(*): 增加interview中模拟instanceOf 的实现

---
+++ interview/interview-code/instanceOf.js
@@ -0,0 +1,30 @@
+ /**
+  * 模拟实现instanceOf - 源知识：对象的继承实质
+  * @param {*} L 
+  * @param {*} R 
+  */
+function instance_Of(L, R) {
+  // 不断遍历L的__proto__来找到和R的prototype进行比对
+  L = L.__proto__;
+  O = R.prototype;
+  let i = 0; // 测试：统计计数用的
+  while (L) {
+    if (L === O) {
+      return true;
+    }
+    L = L.__proto__;
+    console.log(`[${i}]L`, L); // 测试：L的最后一层__proto__指向null
+    i++; // 测试
+  }
+  return false;
+}
+
+/**
+ * 测试
+ */
+function test(L, R, fn) {
+  console.log(`${L} 是否instanceof ${R}`, instance_Of(L, R));
+}
+
+test([], Array);
+test(function() {}, Array);
