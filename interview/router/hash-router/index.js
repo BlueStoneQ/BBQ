8b3091823fbeb095697384a6fb74b61dd6224159
乔阳 <sTone@jinxianshengdeMacBook-Pro.local>
2019-09-24 16:02:54 +0800

    feat(hash-router): 实现hash-router的框架 + 刷新+订阅功能

--- interview/router/hash-router/index.js
+++ interview/router/hash-router/index.js
@@ -1,6 +1,21 @@
+/**
+ * hash 路由
+ */
 class Router {
+  // 路由基本数据结构
   constructor () {
     this.routes = {};
     this.currentUrl = '';
+  }
+  // 订阅：用相应的回调函数callback订阅对应的path
+  route(path, callback) {
+    this.routes[path] = callback || function() {};
+  }
+  // 功能：刷新
+  refresh() {
+    // 获取当前路径中的hash路径
+    this.currentUrl = location.hash.slice(1) || '/';
+    // 用该hash为索引去触发routes中对应的回调
+    this.routes[currentUrl]();
   }
 }
