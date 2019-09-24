0c08d5b00d29db27ff110ffb0b0d4f20379634ae
乔阳 <sTone@jinxianshengdeMacBook-Pro.local>
2019-09-24 17:09:24 +0800

    fix(hash-router):  修复对后退行为的辨识

--- interview/router/hash-router/index.js
+++ interview/router/hash-router/index.js
@@ -10,9 +10,11 @@ class Router {
   constructor () {
     this.routes = {};
     this.currentUrl = '';
-    this.histroty = []; // 模拟H5的history：用一个数组记录出现过的hash
+    this.history = []; // 模拟H5的history：用一个数组（栈）记录出现过的hash
     // 用来读取/操作history的指针（下标），根据前进/后退来指定history中的不通hash
-    this.currentIndex = this.histroty.length - 1; 
+    this.currentIndex = this.history.length - 1; 
+    // 开关：标识是否为后退行为: 默认操作不是后退行为
+    this.isBack = false;
     // 初始化 
     // 监听浏览器部分事件：增加与浏览器行为的联动
     window.addEventListener('load', this.refresh, false);
@@ -26,20 +28,32 @@ class Router {
   refresh() {
     // 获取当前路径中的hash路径
     this.currentUrl = location.hash.slice(1) || '/';
-    // 对history的同步
-    this.histroty.push(currentUrl);
-    this.currentIndex++;
+    if (!this.isBack) {
+      // 非后退行为
+      if(this.currentIndex < this.history.length - 1) {
+        // 并且currentIndex没有指向history的栈顶
+        // 我们把以currentIndex为界限把history进行截取并更新到this.history上
+        // 此操作来避免当点击后退按钮之后,再进行正常跳转,指针会停留在原地,而数组添加新hash路由
+        this.history = this.history.slice(0, this.currentIndex + 1);
+      }
+      // 对history的同步 - 如果是后退行为 就不用history入栈这个操作了
+      this.history.push(currentUrl);
+      this.currentIndex++;
+    }
     // 用该hash为索引去触发routes中对应的回调
     this.routes[currentUrl]();
+    // 恢复开关 迎接下一轮操作
+    this.isBack = false;
   }
   // 后退功能
   backOff() {
-    // 同步this.currentIndex
+    // 同步this.currentIndex - 这里之所以移动指针读取history给current,而不是直接对history进行出栈操作：因为除了后退，还有前进哈哈
     this.currentIndex = this.currentIndex <= 0 ? 0 : this.currentIndex - 1;
-    this.currentUrl = this.histroty[this.currentIndex];
     // 同步location.hash
-    location.hash = `#${this.currentUrl}`;
-    // 触发对应路由订阅的事件
-    this.route[this.currentUrl]();
+    location.hash = `#${this.history}`;
+    // 后退标志打开
+    this.isBack = true;
+    // 刷新
+    this.refresh();
   }
 }
