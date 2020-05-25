3b4f397a7c0ec9ffce25923fc72965c88f8a9d6c
BlueStoneQ <2282907674@qq.com>
2020-05-25 10:38:15 +0800

    feat(code-like/koa-like): 异步compose实现

--- code-like/koa-like/koa-compose.js
+++ code-like/koa-like/koa-compose.js
@@ -42,6 +42,62 @@ const compose = (middlewares) => () => {
   return ret;
 }
 
+
+
+/**
+ * ***************************************************************************************
+ */
+
+/**
+ * test 异步函数组合
+ */
+const fnA1 = async (next) => {
+  console.log('fnA1')
+  next && await next();
+  console.log('fnA1 end')
+}
+
+const fnA2 = async (next) => {
+  console.log('fnA2')
+  next && await next();
+  console.log('fnA2 end')
+}
+
+const fnA3 = async (next) => {
+  console.log('fnA3')
+  // await本质上就是调用next的then方法
+  next && await next();
+  console.log('fnA3 end')
+}
+
+/**
+ * compose的实现核心方案：
+ *    Promise对于异步流程的控制
+ *    + disPatch的递归调用（实现原理是：迭代器，不断指向新的middleware，有一个现成的reduce实现迭代器，这里的遍历实现利用的还是递归） 
+ *    + （i作为index去遍历,可以理解为游标法）
+ *    + 体会下包装函数的作用：经过包装的函数 可以被我们按照我们需要的方式处理 在最后只需要return原函数即可
+ * promise.resolve的作用就是把里面的东西放到了then中（处理链式调用，或者异步调用），也就是await的后面：dispatch其实就是做了这个工作
+ * 体会递归对于嵌套遍历调用和流程控制的意义
+ * @returns {Function} 高阶函数, 该返回值函数的返回值才是真正的值, 该高阶函数就是这里中间件队列调用的启动器（就像导火索一样）
+ */
+const composeAsync = (middlewares) => () =>  {
+  // 嵌套遍历器入口-启动
+  return dispatch(0);
+  /**
+   * 构造一个dispatch函数
+   * 这个dispatch函数就是把中间件包装为一个高阶函数，中间件作为返回值（被包裹在了promise.resolve中），同时利用递归遍历（嵌套）执行中间件
+   * @param {Number} i 中间件队列的游标
+   */
+  function dispatch(i) {
+    let fn = middlewares[i];
+    // 终止递归条件
+    if (!fn) return Promise.resolve();
+    return Promise.resolve(fn(function next() {
+      return dispatch(i + 1);
+    }));
+  }
+}
+
 /**
  * test
  */
@@ -50,4 +106,9 @@ const middlewares = [fn1, fn2, fn3];
 const finalFn = compose(middlewares);
 // 启动了遍历执行middleware的过程
 finalFn();
+
 // test 异步函数组合
+const asnycMiddlewares = [fnA1, fnA2, fnA3];
+const finalFnAsync = composeAsync(asnycMiddlewares);
+// 启动了遍历执行middleware的过程
+finalFnAsync();
