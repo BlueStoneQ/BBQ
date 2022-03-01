5dad3c9f5e0712ac79b6b90b5bcebe6ffc373521
BlueStoneQ <2282907674@qq.com>
2022-03-01 15:21:45 +0800

    feat: apply

--- war2022/jsWriteByHand/call-apply-bind.js
+++ war2022/jsWriteByHand/call-apply-bind.js
@@ -6,6 +6,7 @@
 const call = (...args) => {
   // 防御
   if (typeof this !== 'function') {
+    console.error('type error');
     return;
   }
 
@@ -13,10 +14,31 @@ const call = (...args) => {
   const context = args.shift() || window;
   // 获取当前函数 并挂载到context上 这样就完成了this的绑定
   context.fn = this;
-  // 执行当前函数 并返回函数返回值
+  // 执行当前函数 
   const result = context.fn(...args);
   // 卸载该属性
   delete context.fn;
+  // 返回函数返回值
+  return result;
+}
+
+
+const apply = (...args) => {
+  // defend
+  if (typeof this !== 'function') {
+    error('type error');
+    return;
+  }
   
+  // 获取context
+  const context = args[0] || window;
+  // 获取当前函数 挂载到context上
+  context.fn = this;
+  // 获取当前参数
+  const args1 = args[1] || [];
+  const result = context.fn(...args1);
+  // 卸载该函数从context上
+  delete context.fn;
+  // return 该函数返回值
   return result;
 }
