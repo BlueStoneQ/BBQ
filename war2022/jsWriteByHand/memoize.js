43f3698b9bbeaf77af67079d02c35e236d71d5fd
BlueStoneQ <2282907674@qq.com>
2022-03-02 17:38:49 +0800

    feat: memoize

--- war2022/jsWriteByHand/memoize.js
+++ war2022/jsWriteByHand/memoize.js
@@ -9,7 +9,6 @@
 /**
  * 备忘录采用闭包实现
  */
-
 const memoize = (fn) => {
   // 这里可以采用对象字面量 也可以直接使用Map
   const memo = new Map(); // 或者也可以挂载到函数句柄上 memoize.memo = new Map()
@@ -19,14 +18,37 @@ const memoize = (fn) => {
     const key = JSON.stringify(args);
     // 缓存中有 就返回缓存值
     if (memo.has(key)) return memo.get(key);
-    // 缓存中没有 调用函数逻辑 返回值进入缓存
+    // 缓存中没有 调用函数逻辑 
     const result = fn(...args);
+    // 返回值进入缓存
     memo.set(key, result);
     // 返回调用后的结果
     return result;
   }
 }
 
-// test 对斐波那契作备忘录
+// test 对斐波那契作备忘录处理
+let count = 0; // 调用次数计数
+
+const fabonaci = (n) => {
+  count++;
+  if (n < 2) return n;
+  return fabonaci(n - 1) + fabonaci(n - 2);
+}
 
 
+for (var i = 0; i <= 10; i++) {
+  fabonaci(i)
+}
+
+console.log('memo before: ', count);
+
+count = 0; // reset 
+
+const memoFab = memoize(fabonaci);
+
+for (var i = 0; i <= 10; i++) {
+  memoFab(i)
+}
+
+console.log('memo after: ', count);
