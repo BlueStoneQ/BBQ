bedc0e107d5dc3dccf695da2f3ffad978410cebd
BlueStoneQ <2282907674@qq.com>
2022-03-17 19:09:33 +0800

    feat: dateFormat

---
+++ war2022/jsWriteByHand/data-handle/1-dateFormat.js
@@ -0,0 +1,37 @@
+/**
+ * 日期格式化函数
+ * 2022-3-17
+ */
+
+/**
+ * 
+ * @param {Date} date 
+ * @param {string} format  yyyy MM dd 年月日
+ */
+const dateFormat = (date, format = 'yyyy/MM/dd') => {
+  // defend
+  if (!date) return '';
+  if (Object.prototype.toString.call(date) !== '[object Date]') return '';
+
+  // inti data
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+
+  // algo
+  format = format.replace(/dd/, day);
+  format = format.replace(/MM/, month);
+  format = format.replace(/yyyy/, year);
+
+  // return 
+  return format;
+}
+
+
+const test = () => {
+  console.log(dateFormat(new Date('2020-12-01'), 'yyyy/MM/dd')) // 2020/12/01
+  console.log(dateFormat(new Date('2020-04-01'), 'yyyy/MM/dd')) // 2020/04/01
+  console.log(dateFormat(new Date('2020-04-01'), 'yyyy年MM月dd日')) // 2020年04月01日
+}
+
+test();
