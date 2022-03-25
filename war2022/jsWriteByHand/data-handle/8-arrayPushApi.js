3cf0c43a157c38c71ae58ef1f4425e2bc49d8960
BlueStoneQ <2282907674@qq.com>
2022-03-25 18:45:47 +0800

    feat: 8-arrayPushApi

---
+++ war2022/jsWriteByHand/data-handle/8-arrayPushApi.js
@@ -0,0 +1,18 @@
+/**
+ * 实现数组的push方法
+ * 2022-3-25
+ */
+
+Array.prototype._push = function() {
+  for (let i = 0; i < arguments.length; i++) {
+    this[this.length] = arguments[i];
+  }
+  return this.length;
+}
+
+
+// test
+const arr = [];
+console.log(arr._push(1, 2, 3, 4));
+
+console.log('arr: ', arr);
