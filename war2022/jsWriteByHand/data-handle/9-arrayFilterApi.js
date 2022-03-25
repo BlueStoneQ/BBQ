681031fba1ede44d14d6448a71bf99ccc0feebaa
BlueStoneQ <2282907674@qq.com>
2022-03-25 18:54:12 +0800

    feat: 9-arrayFilterApi

---
+++ war2022/jsWriteByHand/data-handle/9-arrayFilterApi.js
@@ -0,0 +1,33 @@
+/**
+ * 实现数组的filter
+ * 2022-3-25
+ */
+Array.prototype._filter = function() {
+  const fn = arguments[0];
+
+  if (typeof fn !== 'function') {
+    throw new Error('参数必须是一个函数');
+  }
+
+  const result = [];
+
+  for (let i = 0; i < this.length; i++) {
+    const item = this[i];
+    if (fn(item)) {
+      result.push(item);
+    }
+  }
+
+  return result;
+}
+
+
+/**
+ * test
+ */
+
+const arr = [0, 1, 2, 3, 4, 5];
+
+const fn = (item) => item > 2;
+
+console.log(arr._filter(fn));
