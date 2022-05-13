dfcb3b73bac0ddf3b81f7f692690640aaec29ab7
BlueStoneQ <2282907674@qq.com>
2022-05-13 09:20:48 +0800

    feat: getArrayItem

---
+++ war2022/面试手写/九坤/getArrayItem.js
@@ -0,0 +1,47 @@
+/**
+ *  九坤-笔试
+ *  2022-5-12
+ *  1. 实现一个函数，这个函数从一个多维数组中查询指定维度的一个项，并返回，多维数组维数不定
+ * 
+ *  me: 其实 可以参考 lodash.get的实现
+ */
+
+/**
+ * 参考lodash.get 这里我们只实现Array的链式取值
+ * @param {*} arr 
+ * @param {*} path '1.2.3' 例如：取[[][2, 3, [1, 2, 3, 4]]]的'1.2.3' 就是取 4
+ * @param {} defaultVal 如果沿着path找不到合法值 则返回指定的defaultValue
+ */
+const getArrayItem = (arr, path, defaultVal = undefined) => {
+  // 1. 没有path的话 arr直接返回
+  if (!path) return arr;
+  // 检查参数 不合规(必须是字符串或者数组) 则抛出error
+  if (typeof path !== 'string' && !Array.isArray(path)) {
+    throw new TypeError('expect typeof of \"path\" to be \"Array\" of \"string\"');
+  }
+  // 格式化path为数组 后面统一用数组参与逻辑
+  if (typeof path !== 'array') {
+    path = path.split('.');
+  }
+
+  let result = arr.slice(); // 避免引用类型污染 - 污染原数组
+
+  for (const p of path) {
+    // 如果path中断了 则返回
+    if (result[p] === undefined) {
+      result = defaultVal;
+      break;
+    }
+    result = result[p];
+  }
+
+  return result;
+}
+
+
+/**
+ * test
+ */
+
+const arr = [0, [1, 2, [3, 4, 5, 6]]];
+console.log(getArrayItem(arr, '1.2.3'));
