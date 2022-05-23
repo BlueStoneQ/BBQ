ad7b0a7359576b117aab5f7972f4f46daa33afa6
qiaoyang <2282907674@qq.com>
2022-05-23 08:39:46 +0800

    feat: report

--- war2022/min-project/min-fst-sdk/src/report.js
+++ war2022/min-project/min-fst-sdk/src/report.js
@@ -1,3 +1,11 @@
-const report = () => {}
+import ReportSDK from 'report-sdk';
+
+const report = (fst) => {
+    const reportSDK = new ReportSDK();
+    const metricManager = reportSDK.MetricMansager();
+    metricManager.setMetric("FST", fst);
+
+    reportSDK.report();
+}
 
 export default report
