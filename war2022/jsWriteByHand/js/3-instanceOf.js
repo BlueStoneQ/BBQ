94219129750df6328ac5d27f7c53dff2e4ff3fd9
qiaoyang <2282907674@qq.com>
2022-06-14 19:21:20 +0800

    fix: instanceOf 寻找原型链的终点

--- war2022/jsWriteByHand/js/3-instanceOf.js
+++ war2022/jsWriteByHand/js/3-instanceOf.js
@@ -22,3 +22,20 @@ function _instanceof(obj, constructor) {
   // return 
   return false;
 }
+
+
+/**
+ * 寻找一下原型链的尽头
+ * 看了下 原型链的终点的确是null
+ * 是这样的：Object.getPrototypeOf(Object.prototype) === null
+ */
+
+const findLastProto = () => {
+  let proto = Object.getPrototypeOf({});
+  console.log(proto);
+
+  while (proto) {
+    proto = Object.getPrototypeOf(proto);
+    console.log(proto);
+  }
+}
