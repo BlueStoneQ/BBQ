08f976a68075cdf9838fcda99d2de6248e3deb10
qiaoyang <2282907674@qq.com>
2022-06-18 15:47:56 +0800

    feat: 三角形

---
+++ war2022/jsWriteByHand/场景应用/15-isCircleRef.js
@@ -0,0 +1,22 @@
+/**
+ * 判断对象是否存在循环引用
+ * 2022-6-17
+ * https://juejin.cn/post/6946136940164939813#heading-68
+ */
+
+/**
+ * 首先这还是一个json-tree的遍历问题 利用递归遍历 - 需要一个Set来判断值是不是存在重复
+ * @param {*} obj 
+ */
+const isCycleObject = (obj) => {
+  
+}
+
+// test
+const a = 1;
+const b = {a};
+const c = {b};
+const o = {d:{a:3},c}
+o.c.b.aa = a;
+
+console.log(isCycleObject(o))
