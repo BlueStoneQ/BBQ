8bb60918a07d6868a982961d8c707359206a848a
qiaoyang <2282907674@qq.com>
2022-06-22 18:38:18 +0800

    feat: debounce

--- war2022/jsWriteByHand/js/6-throttle+debounce.js
+++ war2022/jsWriteByHand/js/6-throttle+debounce.js
@@ -62,18 +62,23 @@ const throttle2 = (fn, delay = 300) => {
  * @param {number} delay 
  * @return {function}
  */
-function debounce(fn, delay = 300) {
+function debounce(fn, delay = 300, immediate = true) {
   let timer = null;
 
   return function() {
+    // init data
+    const context = this, args = arguments;
+    
+    // 有需要的话-可以第一次立即执行
+    if (immediate && !timer) {
+      fn && fn.apply(context, args);
+    }
+
     // 清除之前的timer 从新开始计时
     if (timer) {
       clearTimeout(timer)
       timer = null; // 需要手动将timer值赋值为null clearTimeout不会做这件事 
     };
-
-    // init data
-    const context = this, args = arguments;
 
     // 设置计时器
     timer = setTimeout(function() {
