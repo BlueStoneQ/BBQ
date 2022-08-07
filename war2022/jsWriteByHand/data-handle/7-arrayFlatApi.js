99b9f82cf909dd383bde3f51bf169157d393478c
qiaoyang <2282907674@qq.com>
2022-08-07 19:01:06 +0800

    feat: flat-reduce实现

--- war2022/jsWriteByHand/data-handle/7-arrayFlatApi.js
+++ war2022/jsWriteByHand/data-handle/7-arrayFlatApi.js
@@ -4,11 +4,11 @@
  */
 
 /**
- * 递归法
+ * 方法1：递归法
  * @param {*} arr 
  * @param {*} depth 
  */
-const _flat = (arr, depth = 1) => {
+const _flat = (arr, depth = 0) => {
   // base case
   if (!Array.isArray(arr) || depth <= 0) {
     return arr;
@@ -21,11 +21,25 @@ const _flat = (arr, depth = 1) => {
     if (Array.isArray(item)) {
       result = result.concat(_flat(item, depth - 1));
     } else {
-      result.push(item);
+      result = result.concat(item);
     }
   }
 
   return result;
+}
+
+/**
+ * 方法2：reduce（代替了遍历） + 递归
+ */
+const _flat2 = (arr, depth = 0) => {
+  // base case
+  if (!Array.isArray(arr) || depth === 0) {
+    return arr;
+  }
+  // 利用reduce遍历 + 遇到item为Array的情况 就需要递归处理
+  return arr.reduce((pre, cur) => {
+    return pre.concat(Array.isArray(cur) ? _flat2(cur, depth - 1) : cur);
+  }, []);
 }
 
 
@@ -35,4 +49,5 @@ const _flat = (arr, depth = 1) => {
 
 const arr = [1, 2, [3, 4, [5, 6, [7, 8]]]];
 
-console.log(_flat(arr, Infinity));
+console.log(_flat(arr, Infinity)); 
+console.log(_flat2(arr, Infinity)); 
