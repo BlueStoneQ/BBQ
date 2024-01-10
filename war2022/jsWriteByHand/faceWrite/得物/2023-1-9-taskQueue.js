4eeaf9d6bd321acad80e4516d952c70d36aab309
qiaoyang <2282907674@qq.com>
2024-01-10 10:57:57 +0800

    feat: note

--- war2022/jsWriteByHand/faceWrite/得物/2023-1-9-taskQueue.js
+++ war2022/jsWriteByHand/faceWrite/得物/2023-1-9-taskQueue.js
@@ -1,23 +1,26 @@
-// date: 2023-1-9 得物 国际化 1面
-// 实现一个 arrange 函数,可以进行时间和工作调度
-// [ > ... ] 表示调用函数后的打印内容
+/**
+date: 2023-1-9 得物 国际化 1面 async task queue
 
-// arrange('William').execute();
-// > William is notified
+实现一个 arrange 函数,可以进行时间和工作调度
+[ > ... ] 表示调用函数后的打印内容
 
-// arrange('William').do('commit').execute();
-// > William is notified
-// > Start to commit
+arrange('William').execute();
+> William is notified
 
-// arrange('William').wait(5).do('commit').execute();
-// > William is notified
-// 等待 5 秒
-// > Start to commit
+arrange('William').do('commit').execute();
+> William is notified
+> Start to commit
 
-// arrange('William').waitFirst(5).do('push').execute();
-// 等待 5 秒
-// > William is notified
-// > Start to push
+arrange('William').wait(5).do('commit').execute();
+> William is notified
+等待 5 秒
+> Start to commit
+
+arrange('William').waitFirst(5).do('push').execute();
+等待 5 秒
+> William is notified
+> Start to push
+*/
 
 // const command = {
 //   COMMIT: 'commit',
@@ -70,29 +73,28 @@ Arrange.prototype.waitFirst = function(delay = 0) {
   this._addFirstTask(() => new Promise(resolve => {
     setTimeout(() => {
       resolve()
-    }, delay)
+    }, delay * 1000)
   }))
 
   return this
 }
 
 Arrange.prototype.execute = function() {
-  const _run = () => {
-    // defend 判空之类
-    if(this.taskQueue.length === 0) return
-    
-    const task = this.taskQueue.shift()
-
-    task().then(res => {
-      _run()
-    }).catch(err => {
-      _run()
-    })
-  }
-
-  _run()
-
+  this._run() 
   return this
+}
+
+Arrange.prototype._run = function() {
+  // defend 判空之类
+  if(this.taskQueue.length === 0) return
+  
+  const task = this.taskQueue.shift()
+
+  task().then(res => {
+    _run()
+  }).catch(err => {
+    _run()
+  })
 }
 
 
